02726536f13459de93ef737f50f3b579b3d8ab70
Enrique <octepc.97@gmail.com>
2022-09-17 10:31:15 -0500

    finished the converter

---
+++ coins-converter/converter.py
@@ -0,0 +1,8 @@
+soles = input('¿Cuántos soles tienes?: ')
+soles = float(soles)
+valor_dolar = 3.95
+dolares = soles / valor_dolar
+dolares = round(dolares, 2)
+dolares = str(dolares)
+
+print('Tienes $'+dolares + ' dólares')
