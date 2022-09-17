9eda35b28b067e50c499f1ab554436b2a6f3abb6
Enrique <octepc.97@gmail.com>
2022-09-17 11:16:20 -0500

    added control flow to the converter

--- coins-converter/converter-to-dollars.py
+++ coins-converter/converter-to-dollars.py
@@ -1,8 +1,42 @@
-soles = input('¿Cuántos soles tienes?: ')
-soles = float(soles)
-valor_dolar = 3.95
-dolares = soles / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
+menu = """
+Bienvenido al conversor de monedas 🙂
 
-print('Tienes $'+dolares + ' dólares')
+1. Soles
+2. Pesos Argentinos
+3. Pesos Mexicanos
+
+Elige una opción: """
+
+opcion = input(menu)
+
+if opcion == '1':
+    soles = input('¿Cuántos soles tienes?: ')
+    soles = float(soles)
+    valor_dolar = 3.95
+    dolares = soles / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+
+    print('Tienes $'+dolares + ' dólares')
+
+elif opcion == '2':
+    soles = input('¿Cuántos pesos argentinos tienes?: ')
+    soles = float(soles)
+    valor_dolar = 65
+    dolares = soles / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+
+    print('Tienes $'+dolares + ' dólares')
+
+elif opcion == '3':
+    soles = input('¿Cuántos pesos mexicanos tienes?: ')
+    soles = float(soles)
+    valor_dolar = 3875
+    dolares = soles / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print('Tienes $'+dolares + ' dólares')
+
+else:
+    print('Ingresa una opción correcta')
---
+++ condicionales.py
@@ -0,0 +1,6 @@
+edad = int(input('Escribe tu edad: '))
+
+if edad > 17:
+    print('Es mayor de dad')
+else:
+    print('Es menor de edad')
