90811fea69032d2faf8cc4708b04755b8f2497e4
Enrique <octepc.97@gmail.com>
2022-09-19 14:00:59 -0500

    made a simple game to guess a number

---
+++ adivina_el_numero.py
@@ -0,0 +1,19 @@
+import random
+
+
+def run():
+    num_aleatorio = random.randint(1, 100)
+    num_elegido = int(input('Elige un número al azar: '))
+    while num_elegido != num_aleatorio:
+        if num_elegido < num_aleatorio:
+            print('Busca un número más grande')
+            num_elegido = int(input('Elige otro número: '))
+
+        else:
+            print('Busca un número más pequeño')
+            num_elegido = int(input('Elige otro número: '))
+    print('¡Ganaste!')
+
+
+if __name__ == '__main__':
+    run()
